edf09b55c887a4f259b12bed0046f3f12599d2ce
Andres Rivera <soulkalir@gmail.com>
2020-07-03 18:56:16 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -68,9 +68,9 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase){
-  return "I can't hear you!"
+  return "I can\'t hear you!"
   } else if (string === string.toUpperCase){
-      return "Yes INDEED!"
+      return "YES INDEED!"
   } else if (string === "I love you, Grandma"){
     return "I love you, too."
   }
