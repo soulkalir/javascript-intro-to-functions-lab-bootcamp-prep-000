bd79666bb362cfc6aa4d8d24f82455b4e9361466
Andres Rivera <soulkalir@gmail.com>
2020-07-03 19:09:23 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -73,6 +73,6 @@ function sayHiToGrandma(string){
   } else if(string === string.toUpperCase()){
     return 'YES INDEED!'
   } else if(string === 'I love you, Grandma.'){
-  console.log('I love you, too.')
+    return('I love you, too.')
 }
 }
